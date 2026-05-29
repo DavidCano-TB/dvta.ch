8fe384f8f2848bdaa9d6dff08ace78db01ee5c3a
dvd <davidcano.ch@gmail.com>
2026-05-29 21:28:59 +0200

    deploy: sync all pending changes

--- _check.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d c:\dvdcoin
-git status --short > c:\dvdcoin\_status.txt 2>&1
-git log --oneline -5 >> c:\dvdcoin\_status.txt 2>&1
--- _push.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d c:\dvdcoin
-git add -A
-git commit -m "fix: asegurar hub con botones Bank y Exams visibles" > c:\dvdcoin\_push_result.txt 2>&1
-git push >> c:\dvdcoin\_push_result.txt 2>&1
-echo DONE >> c:\dvdcoin\_push_result.txt
